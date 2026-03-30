b15110ee01e9401c0657b006d31d8b4865b8e9ba
Helene Athalia <g00440625@atu.ie>
2026-03-30 21:03:31 +0100

    add logic to create random password and display it

--- script.js
+++ script.js
@@ -26,3 +26,37 @@ lengthSlider.addEventListener("input", () => {
 // Generate password when button is clicked
 generateButton.addEventListener("click", makePassword);
 
+function makePassword() {
+    const length = Number(lengthSlider.value);
+    const includeUppercase = uppercaseCheckbox.checked;
+    const includeLowercase = lowercaseCheckbox.checked;
+    const includeNumbers = numbersCheckbox.checked;
+    const includeSymbols = symbolsCheckbox.checked;
+
+    //User must select at least one character type
+    if (!includeUppercase && !includeLowercase && !includeNumbers && !includeSymbols) {
+        alert("Please select at least one character type!");
+        return;
+    }
+
+    const newPassword = createRandomPassword(length, includeUppercase, includeLowercase, includeNumbers, includeSymbols);
+    passwordInput.value = newPassword;
+}
+
+function createRandomPassword(length, includeUppercase, includeLowercase, includeNumbers, includeSymbols) {
+    let characterPool = "";
+
+    if (includeUppercase) characterPool += uppercaseLetters;
+    if (includeLowercase) characterPool += lowercaseLetters;
+    if (includeNumbers) characterPool += numberCharacters;
+    if (includeSymbols) characterPool += symbolCharacters;  
+
+    let password = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characterPool.length);
+        password += characterPool[randomIndex];
+    }
+
+    return password;
+}
